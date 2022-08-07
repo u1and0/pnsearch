bd79a5f058a4f6b227ab7a3348bce5444b95e559
u1and0 <e01.ando60@gmail.com>
2022-08-08 07:21:31 +0900

    [add] QFrame for table ref

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/mattn/go-sqlite3 v1.14.14
+	github.com/tobgu/qframe v0.3.6
 )
 
 require (
@@ -16,6 +17,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mauricelam/genny v0.0.0-20190320071652-0800202903e5 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/tobgu/qframe"
+	qsql "github.com/tobgu/qframe/config/sql"
 )
 
 const (
@@ -22,54 +24,53 @@ const (
 
 var (
 	showVersion bool
-	table       Table
+	qf          qframe.QFrame
 )
 
 type (
-	Table       []Row
-	NullableRow struct {
-		// OrderNo string// 製番
-		// ProductNo string// 製番_品名
-		// UnitNo string // ユニットNo
-		// Pid string // 品番
-		// Name string // 品名
-		// Type string // 形式寸法
-		// Unit string // 単位
-		// Quantity int // 仕入原価数量
-		// // 仕入原価単価
-		// // 入原価金額
-		// // 在庫払出数量
-		// // 在庫払出単価
-		// // 在庫払出金額
-		// // 登録日
-		// // 発注日
-		// // 納期
-		// // 回答納期
-		// // 納入日
-		// // 発注区分
-		// // メーカ
-		// Product string // 材質
-		// /Quantity int / 員数
-		// // 必要数
-		// // 部品発注数
-		// // 発注残数
-		// // 発注単価
-		// // 発注金額
-		// // 進捗レベル
-		// // 工程名
-		// // 仕入先略称
-		// // オーダーNo
-		// // 納入場所名
-		// // 部品備考
-		// // 原価費目ｺｰﾄﾞ
-		// // 原価費目名
-		// 		OrderNum string
-		// 		ProductNum
-
-		Pid  sql.NullString
-		Name sql.NullString
-		Type sql.NullString
-	}
+	Table []Row
+	// Table struct {
+	// OrderNo string// 製番
+	// ProductNo string// 製番_品名
+	// UnitNo string // ユニットNo
+	// Pid string // 品番
+	// Name string // 品名
+	// Type string // 形式寸法
+	// Unit string // 単位
+	// Quantity int // 仕入原価数量
+	// // 仕入原価単価
+	// // 入原価金額
+	// // 在庫払出数量
+	// // 在庫払出単価
+	// // 在庫払出金額
+	// // 登録日
+	// // 発注日
+	// // 納期
+	// // 回答納期
+	// // 納入日
+	// // 発注区分
+	// // メーカ
+	// Product string // 材質
+	// /Quantity int / 員数
+	// // 必要数
+	// // 部品発注数
+	// // 発注残数
+	// // 発注単価
+	// // 発注金額
+	// // 進捗レベル
+	// // 工程名
+	// // 仕入先略称
+	// // オーダーNo
+	// // 納入場所名
+	// // 部品備考
+	// // 原価費目ｺｰﾄﾞ
+	// // 原価費目名
+	// 		OrderNum string
+	// 		ProductNum
+	// Pid  sql.NullString
+	// Name sql.NullString
+	// Type sql.NullString
+	// }
 	Row struct {
 		Pid  string
 		Name string
@@ -83,54 +84,6 @@ type (
 )
 
 func init() {
-	var (
-		db, err = sql.Open("sqlite3", FILENAME)
-		sqlQ    = fmt.Sprintf(`SELECT %s, %s, %s
-						FROM "order2"
-						`, "品番", "品名", "形式寸法")
-	)
-	rows, err := db.Query(sqlQ)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	// Null処理
-	for rows.Next() {
-		r := new(NullableRow)
-		if err := rows.Scan(&r.Pid, &r.Name, &r.Type); err != nil {
-			log.Fatal(err)
-		}
-
-		rv := Row{
-			Pid:  r.Pid.String,
-			Name: r.Name.String,
-			Type: r.Type.String,
-		}
-
-		table = append(table, rv)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return
-}
-
-func (q *Query) search() (result Table) {
-	for _, r := range table {
-		bol := strings.Contains(r.Pid, q.Pid) &&
-			strings.Contains(r.Name, q.Name) &&
-			strings.Contains(r.Type, q.Type)
-		if bol {
-			result = append(result, r)
-			fmt.Println(r)
-		}
-	}
-	return
-}
-
-func main() {
 	flag.BoolVar(&showVersion, "v", false, "Show version")
 	flag.Parse()
 	if showVersion {
@@ -138,6 +91,18 @@ func main() {
 		os.Exit(0) // Exit with version info
 	}
 
+	db, err := sql.Open("sqlite3", FILENAME)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	qf = qframe.ReadSQL(tx, qsql.Query("select * from order2"), qsql.SQLite())
+}
+
+func main() {
 	// Router
 	r := gin.Default()
 	r.Static("/static", "./static")
@@ -145,31 +110,80 @@ func main() {
 
 	// API
 	r.GET("/", func(c *gin.Context) {
+		view := []qframe.StringView{
+			qf.MustStringView("品番"),
+			qf.MustStringView("品名"),
+			qf.MustStringView("形式寸法"),
+		}
+		table := Table{}
+		pid := toSlice(view[0])
+		name := toSlice(view[1])
+		typed := toSlice(view[2])
+		// len(pid)仮
+		for i := 0; i < 10; i++ {
+			r := Row{
+				Pid:  pid[i],
+				Name: name[i],
+				Type: typed[i],
+			}
+			table = append(table, r)
+		}
+		fmt.Println(table)
 		c.HTML(http.StatusOK, "table.tmpl", gin.H{
 			"msg":   "トップから10件を表示",
-			"table": table[:10],
-		})
-	})
-	r.GET("/search", func(c *gin.Context) {
-		q := new(Query)
-		if err := c.ShouldBind(q); err != nil {
-			c.HTML(http.StatusBadRequest, "table.tmpl", gin.H{
-				"msg": "Bad Query",
-			})
-			return
-		}
-		fmt.Printf("%+v\n", q)
-		if len(table) == 0 {
-			c.HTML(http.StatusBadRequest, "table.tmpl", gin.H{
-				"msg": "検索結果がありません",
-			})
-			return
-		}
-		c.HTML(http.StatusOK, "table.tmpl", gin.H{
-			"msg":   fmt.Sprintf("%#v を検索, 30件を表示", q),
-			"table": q.search(),
+			"table": table,
 		})
 	})
 
+	// r.GET("/search", func(c *gin.Context) {
+	// 	table := Table{}
+	// 	q := new(Query)
+	// 	if err := c.ShouldBind(q); err != nil {
+	// 		c.HTML(http.StatusBadRequest, "table.tmpl", gin.H{
+	// 			"msg": "Bad Query",
+	// 		})
+	// 		return
+	// 	}
+	// 	fmt.Printf("%+v\n", q)
+	// 	if len(table.品番) == 0 {
+	// 		c.HTML(http.StatusBadRequest, "table.tmpl", gin.H{
+	// 			"msg": "検索結果がありません",
+	// 		})
+	// 		return
+	// 	}
+	// 	c.HTML(http.StatusOK, "table.tmpl", gin.H{
+	// 		"msg":   fmt.Sprintf("%#v を検索, 30件を表示", q),
+	// 		"table": q.search(),
+	// 	})
+	// })
+
 	r.Run()
+}
+
+// func (q *Query) search() (result Table) {
+// 	table := Table{}
+// 	for _, r := range table {
+// 		bol := strings.Contains(r.Pid, q.Pid) &&
+// 			strings.Contains(r.Name, q.Name) &&
+// 			strings.Contains(r.Type, q.Type)
+// 		if bol {
+// 			result = append(result, r)
+// 			fmt.Println(r)
+// 		}
+// 	}
+// 	return
+// }
+//
+func toString(ptr *string) string {
+	if (ptr == nil) || reflect.ValueOf(ptr).IsNil() {
+		return ""
+	}
+	return *ptr
+}
+
+func toSlice(view qframe.StringView) (stringSlice []string) {
+	for _, v := range view.Slice() {
+		stringSlice = append(stringSlice, toString(v))
+	}
+	return
 }
