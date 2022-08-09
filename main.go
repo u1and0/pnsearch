59b2943e8e5fe7542fcd17f96191ae2edb9acb94
u1and0 <e01.ando60@gmail.com>
2022-08-09 10:18:11 +0900

    [rm] comment, useless commands

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ const (
 	// VERSION : version info
 	VERSION = "v0.1.0"
 	// FILENAME = "./test/test50row.db"
-	FILENAME = "/mnt/2_Common/06_ツール/software/python/jupyter/PNsearch/data/sqlite3.db"
+	FILENAME = "./data/sqlite3.db"
 	// SQLQ : 実行するSQL文
 	SQLQ = `SELECT
 			製番,
@@ -34,8 +34,11 @@ const (
 			必要数,
 			部品発注数
 			FROM order2
-			WHERE rowid > 800000
 			`
+	// WHERE rowid > 800000
+
+	// MAXROW : qfからTableへ変換する最大行数
+	MAXROW = 1000
 )
 
 var (
@@ -44,55 +47,16 @@ var (
 )
 
 type (
+	// Table : HTMLへ書き込むための行指向のstruct
 	Table []Row
-	// Table struct {
-	// OrderNo string// 製番
-	// ProductNo string// 製番_品名
-	// UnitNo string // ユニットNo
-	// Pid string // 品番
-	// Name string // 品名
-	// Type string // 形式寸法
-	// Unit string // 単位
-	// Quantity int // 仕入原価数量
-	// // 仕入原価単価
-	// // 入原価金額
-	// // 在庫払出数量
-	// // 在庫払出単価
-	// // 在庫払出金額
-	// // 登録日
-	// // 発注日
-	// // 納期
-	// // 回答納期
-	// // 納入日
-	// // 発注区分
-	// // メーカ
-	// Product string // 材質
-	// /Quantity int / 員数
-	// // 必要数
-	// // 部品発注数
-	// // 発注残数
-	// // 発注単価
-	// // 発注金額
-	// // 進捗レベル
-	// // 工程名
-	// // 仕入先略称
-	// // オーダーNo
-	// // 納入場所名
-	// // 部品備考
-	// // 原価費目ｺｰﾄﾞ
-	// // 原価費目名
-	// 		OrderNum string
-	// 		ProductNum
-	// Pid  sql.NullString
-	// Name sql.NullString
-	// Type sql.NullString
-	// }
+	// Row : Tableの一行
 	Row struct {
 		UnitNo string
 		Pid    string
 		Name   string
 		Type   string
 	}
+	// Query : URLクエリパラメータ
 	Query struct {
 		UnitNo string `form:"要求番号"`
 		Pid    string `form:"品番"`
@@ -112,6 +76,7 @@ func init() {
 }
 
 // DB in memory
+// const のSQLQで読み込まれる全データをqf に読み込む。
 func init() {
 	db, err := sql.Open("sqlite3", FILENAME)
 	if err != nil {
@@ -136,7 +101,7 @@ func main() {
 		table := Frame2Table(qf)
 		fmt.Println(table)
 		c.HTML(http.StatusOK, "table.tmpl", gin.H{
-			"msg":   "トップから10件を表示",
+			"msg":   fmt.Sprintf("テストページ / トップから%d件を表示", len(table)),
 			"table": table,
 		})
 	})
@@ -151,7 +116,7 @@ func main() {
 		}
 		fmt.Printf("query: %#v\n", q)
 
-		// Search word
+		// Search keyword by query parameter
 		filtered := q.search()
 		table := Frame2Table(filtered)
 		if len(table) == 0 {
@@ -168,15 +133,6 @@ func main() {
 
 	r.Run()
 }
-
-// filterConstructor : 正規表現で検索するfilterを生成
-// func filterConstructor(q, col string) qframe.Filter {
-// 	re := regexp.MustCompile(ToRegex(q))
-// 	return qframe.Filter{
-// 		Comparator: func(p *string) bool { return re.MatchString(toString(p)) },
-// 		Column:     col,
-// 	}
-// }
 
 func (q *Query) search() qframe.QFrame {
 	res := []*regexp.Regexp{
@@ -203,22 +159,7 @@ func (q *Query) search() qframe.QFrame {
 			Comparator: func(p *string) bool { return res[3].MatchString(toString(p)) },
 			Column:     "形式寸法",
 		},
-		// filterConstructor(q.Pid, "品番"),
-		// filterConstructor(q.Name, "品名"),
-		// filterConstructor(q.Type, "形式寸法"),
 	}
-
-	// re := regexp.MustCompile(ToRegex(q.Name)) //`.*A.*00.*`)
-	// filter := qframe.Filter{
-	// 	Comparator: func(p *string) bool { return re.MatchString(toString(p)) },
-	// 	Column:     "品名",
-	// }
-	// re2 := regexp.MustCompile(ToRegex(q.Pid)) //`.*GAA.*`)
-	// filter2 := qframe.Filter{
-	// 	Comparator: func(p *string) bool { return re2.MatchString(toString(p)) },
-	// 	Column:     "品番",
-	// }
-	fmt.Println(filters)
 	return qf.Filter(qframe.And(filters...))
 }
 
@@ -258,7 +199,7 @@ func Frame2Table(qf qframe.QFrame) (table Table) {
 	}
 	// NameとTypeは常に表示する仕様
 	for i := 0; i < len(slices[2]); i++ {
-		if i >= 1000 { // 最大1000件表示
+		if i >= MAXROW { // 最大1000件表示
 			break
 		}
 		r := Row{
